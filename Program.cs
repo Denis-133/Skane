fbc711a2477acaa55e2715d575873841b2c88283
”Denis <eliseev_denis_95@mail.ru>
2021-01-21 20:55:07 +0400

    lesson-6/class-List

--- Program.cs
+++ Program.cs
@@ -11,14 +11,48 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point P1 = new Point(1,2,'*');
-            P1.Draw();
+            Point p1 = new Point(10,2,'*');
+            Point p2 = new Point(5,5,'*');
+            Point p3 = new Point(5, 6, '#');
+            Point p4 = new Point(5, 7, '@');
 
-            Point P2 = new Point(5,5,'!');
-            P2.Draw();
+            p1.Draw();
 
-            Console.ReadLine();
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            /*for (int i=0; i<numList.Count; i++) {
+                Console.WriteLine(i);
+            }*/
+            foreach (int i in numList) {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
         
-        }        
+            List <char> Sym = new List<char>() { '*', '*', '#', '*'};
+            foreach (char i in Sym)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
+        }
     }
 }
